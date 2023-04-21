dcc10f8e8d352fa6a30fb56575e9f0555a908300
Atharv Chandratre <atharvchandratre@gmail.com>
2023-04-20 23:26:27 -0500

    Trying to print lines to see where the error is

--- scripts/label_data.py
+++ scripts/label_data.py
@@ -39,6 +39,7 @@ def label_converter(args, inp):
     return_val = []
     for line in inp:
         row = []
+        print(line)
         for c in line:
             if c=='0' or c=='1':
                 row.append(int(c))
