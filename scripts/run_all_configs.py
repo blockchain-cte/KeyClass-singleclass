eb1df7662525b7d42dea43d9a8c82a4addef7087
Atharv Chandratre <atharvchandratre@gmail.com>
2023-04-25 00:35:26 -0500

    multi-run script

--- scripts/run_all_configs.py
+++ scripts/run_all_configs.py
@@ -3,7 +3,7 @@ import subprocess
 base_filename = "config_mimic_"
 base_file_extension = '.yaml'
 
-for i in range(1,20):
+for i in range(0,19):
     filename = base_filename + str(i) + base_file_extension
     print("Running ",filename)
     subprocess.run(["python","run_all.py","--config",filename])
