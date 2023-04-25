96638ae0fe8da5e1cdcf7b2f1d4ed87e78204f6b
Atharv Chandratre <atharvchandratre@gmail.com>
2023-04-24 22:55:38 -0500

    testing class 0

--- keyclass/utils.py
+++ keyclass/utils.py
@@ -116,8 +116,8 @@ def compute_metrics(y_preds: np.array,
     acc = accuracy_score(y_true, (y_preds>theta).astype(int))
     print("Acc",acc)
 
-    prec2, rec2, f1_2, _ = precision_recall_fscore_support(y_true, y_preds_default)
-    acc2 = accuracy_score(y_true, y_preds_default)
+    # prec2, rec2, f1_2, _ = precision_recall_fscore_support(y_true, y_preds_default)
+    # acc2 = accuracy_score(y_true, y_preds_default)
 
     return [
         F1,
