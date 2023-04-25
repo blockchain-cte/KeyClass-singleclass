94cb6852357177f265ef15b555533c342776adde
Atharv Chandratre <atharvchandratre@gmail.com>
2023-04-25 00:04:04 -0500

    class = 0

--- keyclass/utils.py
+++ keyclass/utils.py
@@ -51,14 +51,32 @@ def log(metrics: Union[List, Dict], filename: str, results_dir: str,
         class_being_tested: str
             Class being tested
     """
+    # return [
+    #     F1,
+    #     prec,
+    #     acc,
+    #     rec,
+    #     theta,
+    #     f1_2,
+    #     acc2,
+    #     prec2,
+    #     rec2
+    # ]
     if isinstance(metrics, list):
-        assert len(metrics) == 3, "Metrics must be of length 3!"
+        # assert len(metrics) == 3, "Metrics must be of length 3!"
         results = dict()
         results['F1'] = metrics[0]
         results['Precision'] = metrics[1]
         results['Accuracy'] = metrics[2]
+        results['Recall'] = metrics[3]
+        results['Theta'] = metrics[4]
+        results['F1 - Default'] = metrics[5]
+        results['Precision - Default'] = metrics[6]
+        results['Accuracy - Default'] = metrics[7]
+        results['Recall - Default'] = metrics[8]
+
     elif isinstance(metrics, np.ndarray):
-        assert len(metrics) == 3, "Metrics must be of length 3!"
+        # assert len(metrics) == 3, "Metrics must be of length 3!"
         results = dict()
         results['F1 (mean, std)'] = metrics[0].tolist()
         results['Precision (mean, std)'] = metrics[1].tolist()
@@ -124,7 +142,13 @@ def compute_metrics(y_preds: np.array,
     return [
         F1,
         prec,
-        acc
+        acc,
+        rec,
+        theta,
+        f1_2,
+        prec2,
+        acc2,
+        rec2
     ]
 
 
